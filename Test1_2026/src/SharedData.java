717826574522ce73859512193a8d2573f593f928
Linoy <linoyco5@gmail.com>
2025-11-02 11:17:40 +0200

    hi

--- Test1_2026/src/SharedData.java
+++ Test1_2026/src/SharedData.java
@@ -11,13 +11,14 @@ public class SharedData
 	private ArrayList<Integer> array;
 	/** A list indicating which members participate in the winning solution */
 	private ArrayList<Boolean> winArray;
-	/** A flag indicating whether a valid solution has been fou */
+	/** A flag indicating whether a valid solution has been found */
 	private boolean flag;
+	/** The target value to reach by combining elements */
 	private final int b;
 	
-	/** jbhkbnkbnknbkbj
-	 * @param array     hvgvg
-	 * @param b    nvhbjv
+	/** Constructs a new instance
+	 * @param array    the list of integers to be processed
+	 * @param b     the target value to reach
 	 */
 	public SharedData(ArrayList<Integer> array, int b) {
 		
@@ -25,43 +26,46 @@ public class SharedData
 		this.b = b;
 	}
 
-	/**
-	 * @return
+	/** Returns the list of boolean values representing the winning subset
+	 * @return  indicating which elements are part of the solution
 	 */
 	public ArrayList<Boolean> getWinArray() {
         return winArray;
     }
 
-    /**
-     * @param winArray
+    /** Sets the winning subset list
+     * @param winArray an indicating which elements are part of the solution
      */
     public void setWinArray(ArrayList<Boolean> winArray) {
         this.winArray = winArray;
     }
 
-    /**
-     * @return
+    /** Returns the list of integers being processed.
+     * @return array containing the numbers
      */
     public ArrayList<Integer> getArray() {
         return array;
     }
 
-	/**
-	 * @return        b
+	/** Returns the target value.
+     * @return the target value b
 	 */
 	public int getB() 
 	{
 		return b;
 	}
 
-	/**
-	 * @return      flag
+	/** Returns the shared flag indicating whether a valid solution has been found.
+     * @return  if a solution was found
 	 */
 	public boolean getFlag() 
 	{
 		return flag;
 	}
 
+	/** Sets the shared flag value.
+     * @param flag if a solution was found
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
