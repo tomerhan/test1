0f6193e3c537b0d8b4a73e9137c44c28cab9204e
Linoy <linoyco5@gmail.com>
2025-11-02 11:09:56 +0200

    hi

--- Test1_2026/src/SharedData.java
+++ Test1_2026/src/SharedData.java
@@ -1,36 +1,62 @@
+import java.util.ArrayList;
+
+/** class represents a shared object used for communication
+ * and synchronization between multiple threads
+ * @author Linoy
+ * @version 1.0
+ */
 public class SharedData 
 {
-	private int [] array;
-	private boolean [] winArray;
+	/** List of numbers being worked on */
+	private ArrayList<Integer> array;
+	/** A list indicating which members participate in the winning solution */
+	private ArrayList<Boolean> winArray;
+	/** A flag indicating whether a valid solution has been fou */
 	private boolean flag;
 	private final int b;
 	
-	public SharedData(int[] array, int b) {
+	/** jbhkbnkbnknbkbj
+	 * @param array     hvgvg
+	 * @param b    nvhbjv
+	 */
+	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
 	}
 
-	public boolean[] getWinArray() 
-	{
-		return winArray;
-	}
+	/**
+	 * @return
+	 */
+	public ArrayList<Boolean> getWinArray() {
+        return winArray;
+    }
 
-	public void setWinArray(boolean [] winArray) 
-	{
-		this.winArray = winArray;
-	}
+    /**
+     * @param winArray
+     */
+    public void setWinArray(ArrayList<Boolean> winArray) {
+        this.winArray = winArray;
+    }
 
-	public int[] getArray() 
-	{
-		return array;
-	}
+    /**
+     * @return
+     */
+    public ArrayList<Integer> getArray() {
+        return array;
+    }
 
+	/**
+	 * @return        b
+	 */
 	public int getB() 
 	{
 		return b;
 	}
 
+	/**
+	 * @return      flag
+	 */
 	public boolean getFlag() 
 	{
 		return flag;
